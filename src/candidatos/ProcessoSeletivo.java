56cd407120bf20ef9e8b4f6e3d7cebe8a6f6fa67
Alexya Mello <elizabethalexya@gmail.com>
2024-09-17 15:05:07 -0300

    Estudando métodos em casos específicos!

--- src/candidatos/ProcessoSeletivo.java
+++ src/candidatos/ProcessoSeletivo.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ProcessoSeletivo {
+package candidatos;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        analisarCandidato(1758.87);
+        analisarCandidato(2140.23);
+        analisarCandidato(2000.0);
+    }
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        } else if (salarioBase == salarioPretendido)
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        else {
+            System.out.println("AGUARDANDO DEMAIS CANDIDATOS");
+        }
+    }
 }
